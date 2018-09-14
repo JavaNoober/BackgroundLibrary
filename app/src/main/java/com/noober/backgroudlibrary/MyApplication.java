aac74bf7ea53bd3e45e166d0bb0de4d1e8fd7720
xiaoqi <812817883@qq.com>
2018-09-14 13:25:35 +0800

    update to 1.2.0
    (cherry picked from commit b8187c1277ee7105df9bd29ab6f87676b010b18e)
    
    # Conflicts:
    #	README.md
    
    (cherry picked from commit 1c8230c)

---
+++ app/src/main/java/com/noober/backgroudlibrary/MyApplication.java
@@ -0,0 +1,53 @@
+package com.noober.backgroudlibrary;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import com.noober.background.BackgroundLibrary;
+
+/**
+ * Created by xiaoqi on 2018/9/13
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
