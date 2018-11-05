600418511947229eb7327f01eeb8a38864959767
xiaoqi <812817883@qq.com>
2018-11-05 14:07:44 +0800

    update to 1.3.1
    (cherry picked from commit 191b704)

--- library/src/main/java/com/noober/background/drawable/ColorStateCreator.java
+++ library/src/main/java/com/noober/background/drawable/ColorStateCreator.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.support.annotation.AttrRes;
 
 import com.noober.background.R;
-import com.noober.background.utils.TypeValueHelper;
 
 public class ColorStateCreator {
 
@@ -23,44 +22,39 @@ public class ColorStateCreator {
         states = new int[textTa.getIndexCount()][];
         colors = new int[textTa.getIndexCount()];
         for (int i = 0; i < textTa.getIndexCount(); i++) {
-            int attr = TypeValueHelper.sAppearanceTextValues.get(textTa.getIndex(i), -1);
-            if (attr == -1) {
-                continue;
-            }
-            int typeIndex = textTa.getIndex(i);
-
+            int attr = textTa.getIndex(i);
             if (attr == R.styleable.text_selector_bl_checkable_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_checkable);
+                setStateColor(textTa, attr, android.R.attr.state_checkable);
             } else if (attr == R.styleable.text_selector_bl_unCheckable_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_checkable);
+                setStateColor(textTa, attr, -android.R.attr.state_checkable);
             } else if (attr == R.styleable.text_selector_bl_checked_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_checked);
+                setStateColor(textTa, attr, android.R.attr.state_checked);
             } else if (attr == R.styleable.text_selector_bl_unChecked_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_checked);
+                setStateColor(textTa, attr, -android.R.attr.state_checked);
             } else if (attr == R.styleable.text_selector_bl_enabled_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_enabled);
+                setStateColor(textTa, attr, android.R.attr.state_enabled);
             } else if (attr == R.styleable.text_selector_bl_unEnabled_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_enabled);
+                setStateColor(textTa, attr, -android.R.attr.state_enabled);
             } else if (attr == R.styleable.text_selector_bl_selected_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_selected);
+                setStateColor(textTa, attr, android.R.attr.state_selected);
             } else if (attr == R.styleable.text_selector_bl_unSelected_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_selected);
+                setStateColor(textTa, attr, -android.R.attr.state_selected);
             } else if (attr == R.styleable.text_selector_bl_pressed_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_pressed);
+                setStateColor(textTa, attr, android.R.attr.state_pressed);
             } else if (attr == R.styleable.text_selector_bl_unPressed_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_pressed);
+                setStateColor(textTa, attr, -android.R.attr.state_pressed);
             } else if (attr == R.styleable.text_selector_bl_focused_textColor) {
-                setStateColor(textTa, typeIndex, android.R.attr.state_focused);
+                setStateColor(textTa, attr, android.R.attr.state_focused);
             } else if (attr == R.styleable.text_selector_bl_unFocused_textColor) {
-                setStateColor(textTa, typeIndex, -android.R.attr.state_focused);
+                setStateColor(textTa, attr, -android.R.attr.state_focused);
             }
         }
         return new ColorStateList(states, colors);
     }
 
-    private void setStateColor(TypedArray selectorTa, int typeIndex, @AttrRes int functionId) {
+    private void setStateColor(TypedArray selectorTa, int attr, @AttrRes int functionId) {
         states[index] = new int[]{functionId};
-        colors[index] = selectorTa.getColor(typeIndex, 0);
+        colors[index] = selectorTa.getColor(attr, 0);
         index++;
     }
 }
--- library/src/main/java/com/noober/background/drawable/SelectorDrawableCreator.java
+++ library/src/main/java/com/noober/background/drawable/SelectorDrawableCreator.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.StateListDrawable;
 import android.support.annotation.AttrRes;
 
 import com.noober.background.R;
-import com.noober.background.utils.TypeValueHelper;
 
 public class SelectorDrawableCreator implements ICreateDrawable {
 
@@ -24,63 +23,59 @@ public class SelectorDrawableCreator implements ICreateDrawable {
         StateListDrawable stateListDrawable = new StateListDrawable();
 
         for (int i = 0; i < selectorTa.getIndexCount(); i++) {
-            int attr = TypeValueHelper.sAppearanceSelectorValues.get(selectorTa.getIndex(i), -1);
-            if (attr == -1) {
-                continue;
-            }
-            int typeIndex = selectorTa.getIndex(i);
+            int attr = selectorTa.getIndex(i);
 
             if (attr == R.styleable.background_selector_bl_checkable_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_checkable);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_checkable);
             } else if (attr == R.styleable.background_selector_bl_unCheckable_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_checkable);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_checkable);
             } else if (attr == R.styleable.background_selector_bl_checked_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_checked);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_checked);
             } else if (attr == R.styleable.background_selector_bl_unChecked_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_checked);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_checked);
             } else if (attr == R.styleable.background_selector_bl_enabled_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_enabled);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_enabled);
             } else if (attr == R.styleable.background_selector_bl_unEnabled_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_enabled);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_enabled);
             } else if (attr == R.styleable.background_selector_bl_selected_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_selected);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_selected);
             } else if (attr == R.styleable.background_selector_bl_unSelected_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_selected);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_selected);
             } else if (attr == R.styleable.background_selector_bl_pressed_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_pressed);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_pressed);
             } else if (attr == R.styleable.background_selector_bl_unPressed_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_pressed);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_pressed);
             } else if (attr == R.styleable.background_selector_bl_focused_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_focused);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_focused);
             } else if (attr == R.styleable.background_selector_bl_unFocused_drawable) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_focused);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_focused);
             } else if (attr == R.styleable.background_selector_bl_focused_hovered) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_hovered);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_hovered);
             } else if (attr == R.styleable.background_selector_bl_unFocused_hovered) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_hovered);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_hovered);
             } else if (attr == R.styleable.background_selector_bl_focused_activated) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, android.R.attr.state_activated);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, android.R.attr.state_activated);
             } else if (attr == R.styleable.background_selector_bl_unFocused_activated) {
-                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, typeIndex, -android.R.attr.state_activated);
+                setSelectorDrawable(typedArray, selectorTa, stateListDrawable, attr, -android.R.attr.state_activated);
             }
         }
         return stateListDrawable;
     }
 
     private void setSelectorDrawable(TypedArray typedArray, TypedArray selectorTa,
-                                     StateListDrawable stateListDrawable, int typeIndex, @AttrRes int functionId) throws Exception {
+                                     StateListDrawable stateListDrawable, int attr, @AttrRes int functionId) throws Exception {
         int color = 0;
         Drawable resDrawable = null;
 
         //这里用try catch先判断是否是颜色而不是直接调用getDrawable，为了方便填入的是颜色时可以沿用其他属性,
         //否则如果是其他资源会覆盖app:corners_radius等其他shape属性设置的效果
         try {
-            color = selectorTa.getColor(typeIndex, 0);
+            color = selectorTa.getColor(attr, 0);
             if (color == 0) {
-                resDrawable = selectorTa.getDrawable(typeIndex);
+                resDrawable = selectorTa.getDrawable(attr);
             }
         } catch (Exception e) {
-            resDrawable = selectorTa.getDrawable(typeIndex);
+            resDrawable = selectorTa.getDrawable(attr);
         }
         if (resDrawable == null && color != 0) {
             GradientDrawable tmpDrawable = DrawableFactory.getDrawable(typedArray);
